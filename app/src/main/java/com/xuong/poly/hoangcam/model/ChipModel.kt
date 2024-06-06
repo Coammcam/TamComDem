3b006d41902719d6eb5ce3c99b49fff03acf8ef3
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-06-06 20:35:05 +0700

    Add new models

---
+++ app/src/main/java/com/xuong/poly/hoangcam/model/ChipModel.kt
@@ -0,0 +1,3 @@
+package com.xuong.poly.hoangcam.model
+
+data class ChipModel(val id: Int, val name: String, val image: Int)
---
+++ app/src/main/java/com/xuong/poly/hoangcam/model/OrderModel.kt
@@ -0,0 +1,4 @@
+package com.xuong.poly.hoangcam.model
+
+class OrderModel(val id: String, val total: Float, val status: Boolean) {
+}
